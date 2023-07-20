7c97c6315bc5f63665297dc8f88f81f3f204b04a
Ashish Dutt <ashishdutt@192.168.1.4>
2023-07-20 19:19:00 +0530

    added details in issue rows

---
+++ UltimatePortfolio/IssueRow.swift
@@ -0,0 +1,20 @@
+//
+//  IssueRow.swift
+//  UltimatePortfolio
+//
+//  Created by Ashish Dutt on 20/07/23.
+//
+
+import SwiftUI
+
+struct IssueRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IssueRow_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueRow()
+    }
+}
